7064a27b7b9e4d9ea02cad3922635a70ded1c862
Binod Acharya <acharyabinod0422@gmail.com>
2023-07-11 18:10:42 -0400

    completed get and post mapping

--- src/main/java/com/example/TheCompHub/controllers/ProductController.java
+++ src/main/java/com/example/TheCompHub/controllers/ProductController.java
@@ -1,12 +1,19 @@
 package com.example.TheCompHub.controllers;
 
+import com.example.TheCompHub.entities.Product;
 import com.example.TheCompHub.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin
 public class ProductController {
     @Autowired
     private final ProductService productService;
@@ -15,9 +22,28 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> getProduct(){
+        if(productService.countProducts() <= 0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(productService.getProducts());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable Integer id){
+        Optional<Product> product = productService.getProductById(id);
+        if(product.isEmpty()){
+            ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(product));
+    }
 
-
+    @PostMapping
+    public ResponseEntity<Product> addUser(@RequestBody Product product) {
+        Product addProduct = productService.addProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addProduct);
+    }
 
 
 
--- src/main/java/com/example/TheCompHub/services/ProductService.java
+++ src/main/java/com/example/TheCompHub/services/ProductService.java
@@ -3,6 +3,10 @@ package com.example.TheCompHub.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.TheCompHub.repositories.ProductRepository;
+import com.example.TheCompHub.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -13,7 +17,25 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public long countProducts(){
+        return productRepository.count();
+    }
+    public Iterable<Product> getProducts(){
+        return productRepository.findAll();
+    }
 
+    public Optional<Product> getProductById(Integer id){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct;
+    }
+
+    public Product addProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public List<Product> addProducts(List<Product> product){
+        return (List<Product>) productRepository.saveAll(product);
+    }
 
 
 
