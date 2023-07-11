c3455d6b10be31af247aa54f74efc807f14cc625
Binod Acharya <acharyabinod0422@gmail.com>
2023-07-11 12:11:13 -0400

    Initial commit

---
+++ src/main/java/com/example/TheCompHub/TheCompHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheCompHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCompHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCompHubApplication.class, args);
+	}
+
+}
