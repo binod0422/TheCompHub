9065b196c5f6138ff8890c3205e59f894876efb5
attaliab <attaliabdesign@gmail.com>
2023-07-17 12:30:18 -0400

    changed new arrivals repository from class to interface

--- src/main/java/com/example/TheCompHub/repositories/NewArrivalsRepository.java
+++ src/main/java/com/example/TheCompHub/repositories/NewArrivalsRepository.java
@@ -1,7 +1,10 @@
 package com.example.TheCompHub.repositories;
 
+
+import com.example.TheCompHub.entities.NewArrivals;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class NewArrivalsRepository {
+public interface NewArrivalsRepository extends CrudRepository<NewArrivals, Integer> {
 }
